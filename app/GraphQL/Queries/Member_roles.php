bff673b6b7fcf4069d87dae4f8b915fc03c04b79
guecha <guechadaniel@gmail.com>
2020-01-08 10:11:40 -0500

    arreglo Member Role

--- app/GraphQL/Queries/Member_roles.php
+++ app/GraphQL/Queries/Member_roles.php
@@ -19,8 +19,8 @@ class Member_roles
      */
     public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
-        $MemberRoles = DB::select('Select * from crosstab(\'SELECT c.name, r.name, r.name FROM contacts c INNER JOIN members m ON c.id = m.contact_id INNER JOIN roles r ON r.id = m.role_id where m.project_id = '.$args['project_id'].'\')
-        AS final_result(name Character varying, role1 Character(120), role2 Character(120), role3 Character(120), role4 Character(120), role5 Character(120), role6 Character(120), role7 Character(120), role8 Character(120))');
+        $MemberRoles = DB::select('Select * from crosstab(\'SELECT c.id, r.id, r.id FROM contacts c INNER JOIN members m ON c.id = m.contact_id INNER JOIN roles r ON r.id = m.role_id where m.project_id = '.$args['project_id'].' ORDER BY 1,2\')
+        AS final_result(contact_id integer, role1 integer, role2 integer, role3 integer, role4 integer, role5 integer, role6 integer, role7 integer, role8 integer)');
         
         return $MemberRoles; 
     }
