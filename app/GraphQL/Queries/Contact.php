310e54ce8433cc4369d92460507cf4750c95713f
Daniel Guecha <guechadaniel@gmail.com>
2020-02-08 23:22:25 -0500

    arreglando consulta de doc_required v1

--- app/GraphQL/Queries/Contact.php
+++ app/GraphQL/Queries/Contact.php
@@ -19,27 +19,27 @@ class Contact
      */
     public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
-        $contact = DB::select('select *
-        from  
-            (select con.folder_id, doc_mem.created_at, doc_mem.updated_at, doc_mem.doc_id, doc_mem.file_id
-                from documents_member as doc_mem 
-                inner join members as mem
-                on   mem.id = doc_mem.member_id
-                inner join contacts as con
-                on   con.id = mem.contact_id		    
-                where mem.contact_id = ? and mem.project_id = ?
-            ) as documentos_integrante
-            right join 
-            (select rol.name as role_name, documents_rol.id as documento_id, name_required_documents
-                from documents_rol 
-                inner join required_documents 
-                on required_documents.id = documents_rol.required_document_id
-                inner join roles as rol
-                on rol.id = documents_rol.role_id
-                where documents_rol.role_id in (select role_id from members where contact_id = ? and project_id = ?)
-            ) as documentos_requeridos
-            on documentos_integrante.doc_id = documentos_requeridos.documento_id', [$args['contact_id'] , $args['project_id'], $args['contact_id'] , $args['project_id']]);
-        
-        return $contact;      
+        //$contact = DB::select('select *
+        //from
+        //    (select con.folder_id, doc_mem.created_at, doc_mem.updated_at, doc_mem.doc_id, doc_mem.file_id
+        //        from documents_member as doc_mem
+        //        inner join members as mem
+        //        on   mem.id = doc_mem.member_id
+        //        inner join contacts as con
+        //        on   con.id = mem.contact_id
+        //        where mem.contact_id = ? and mem.project_id = ?
+        //    ) as documentos_integrante
+        //    right join
+        //    (select rol.name as role_name, documents_rol.id as documento_id, name_required_documents
+        //        from documents_rol
+        //        inner join required_documents
+        //        on required_documents.id = documents_rol.required_document_id
+        //        inner join roles as rol
+        //        on rol.id = documents_rol.role_id
+        //        where documents_rol.role_id in (select role_id from members where contact_id = ? and project_id = ?)
+        //    ) as documentos_requeridos
+        //    on documentos_integrante.doc_id = documentos_requeridos.documento_id', [$args['contact_id'] , $args['project_id'], $args['contact_id'] , $args['project_id']]);
+
+        return $contact;
     }
 }
