e21b0c8c3a1ff1d79f3b48f88002db671464c2d5
guecha <guechadaniel@gmail.com>
2020-02-14 11:35:46 -0500

    mejoras proceso envio de correos en compra a proveedores v2

---
+++ resources/views/mails/BuyOrder.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <title>Orden de Compra</title>
+</head>
+<body>
+    <p>Muy cordialmente me dirijo a ustedes para efectuar la siguiente Orden de compra</p>
+    <ul>
+        <a href="https://drive.google.com/open?id={{$document_reference->drive_id}}">Descargar Orden de compra</a>
+    </ul>
+
+    <p>Gracias,</p>
+    
+    <p>APP CONTROL INGENIERIA</p>
+</body>
+</html>
