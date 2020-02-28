66f817f30ee9b039c80c07dfacf0d2eff4d1c467
Daniel Guecha <guechadaniel@gmail.com>
2020-02-28 15:50:45 -0500

    allContacts for state

--- app/Repositories/ContactRepository.php
+++ app/Repositories/ContactRepository.php
@@ -12,9 +12,9 @@ class ContactRepository extends BaseRepository
     {
         return new User;
     }
-    
+
     //public function lastContact()
-    //{        
+    //{
     //    //Trae ultimo contacto registrado
     //    $reference = DB::select('SELECT id FROM contacts ORDER BY id DESC LIMIT 1'); //User::max('id')
     //    return $reference[0];
