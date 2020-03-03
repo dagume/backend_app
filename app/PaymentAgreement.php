88784d67a80c4be2d1ca2943b329d7288ec12024
Dagume <guechadaniel@gmail.com>
2020-03-03 10:35:14 -0500

    PaymentAgreement v2

---
+++ app/PaymentAgreement.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentAgreement extends Model
+{
+    protected  $table= 'payment_agreement';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'order_id',
+        'pay_date',
+        'amount',
+        'state'
+        //'next_pay'
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo('App\Order','order_id');
+    }
+}
