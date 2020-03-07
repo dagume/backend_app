f6d3e44e7a661340ad7bd052cbb7ae24f08389ad
Dagume <guechadaniel@gmail.com>
2020-03-07 11:57:56 -0500

    QUERY REQUIREDDOCUMENTS

--- app/GraphQL/Queries/Contact.php
+++ app/GraphQL/Queries/Contact.php
@@ -20,18 +20,19 @@ class Contact
     public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
         $contact = DB::select('
-        select req_doc.id as doc_id, doc_upload.folder_id, doc_upload.created_at, doc_upload.updated_at, doc_upload.drive_id, req_doc.name_required_documents from(select con.folder_id, doc_con.created_at, doc_con.updated_at, doc_con.doc_id, doc_ref.drive_id
+        select req_doc.id as doc_id, doc_upload.folder_id, doc_upload.created_at, doc_upload.updated_at, doc_upload.drive_id, req_doc.name_required_documents, req_doc.role_id from
+(select con.folder_id, doc_con.created_at, doc_con.updated_at, doc_con.doc_id, doc_ref.drive_id
             from documents_contact as doc_con
             inner join contacts as con
             on   con.id = doc_con.con_id
             inner join document_reference as doc_ref
             on   doc_ref.doc_id = doc_con.id
             where doc_con.con_id = ?) as doc_upload
-            right join (select doc_rol.id, req_doc.name_required_documents from documents_rol as doc_rol
+            right join (select doc_rol.id, req_doc.name_required_documents, doc_rol.role_id as role_id from documents_rol as doc_rol
             inner join required_documents as req_doc
-            on doc_rol.required_document_id = req_doc.id where doc_rol.role_id in (select role_id from members where contact_id = ? and project_id = ?))
+            on doc_rol.required_document_id = req_doc.id where doc_rol.role_id in (select role_id from members where contact_id = ? ))
             as req_doc
-            on req_doc.id = doc_upload.doc_id', [$args['contact_id'], $args['contact_id'] , $args['project_id']]);
+            on req_doc.id = doc_upload.doc_id', [$args['contact_id'], $args['contact_id']]);
 
         return $contact;
     }
