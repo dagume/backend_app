8c5f40ae606f2007b29cce674ff3df5728d3f63f
dagume <guechadaniel@gmail.com>
2020-04-08 17:01:16 -0500

    solucionador para queries acuerdos de pago v1

--- app/Repositories/PaymentAgreementRepository.php
+++ app/Repositories/PaymentAgreementRepository.php
@@ -15,16 +15,16 @@ class PaymentAgreementRepository extends BaseRepository
     public function todayPaymentAgreement($date){
         //Traemos los acuerdos de pagos que vencen hoy
         $data = DB::select('select * from payment_agreement where pay_date = ? and  state = false',[$date]);
-        return $data;
+        return $this->getObjects($data);
     }
     public function betweenPaymentAgreement($first_day, $last_day){
         //Traemos los acuerdos de pago que vencen hoy
         $data =  DB::select('select * from payment_agreement where pay_date between ? and ? and state = false',[$first_day, $last_day]);
-        return $data;
+        return $this->getObjects($data);
     }
     public function getPaymentAgreementsDate($project_id, $pay_date){
         //acuerdos de pago segun fecha dentro de un proyecto
-        $data =  DB::select('select p_a.id, p_a.pay_date, p_a.amount, p_a.state, p_a.order_id, o.name from projects as p 
+        $data =  DB::select('select p_a.id, p_a.pay_date, p_a.amount, p_a.state, p_a.order_id, o.name from projects as p
         inner join orders as o on p.id = o.project_id
         inner join payment_agreement as p_a on o.id = p_a.order_id
         where o.project_id = ? and p_a.pay_date = ?',[$project_id, $pay_date]);
@@ -32,7 +32,7 @@ class PaymentAgreementRepository extends BaseRepository
     }
     public function getprojectPayAgreMonth($project_id, $first_date, $end_date){
         //acuerdos de pago en rango de fechas dentro de un proyecto
-        $data =  DB::select('select p_a.id, p_a.pay_date, p_a.amount, p_a.state, p_a.order_id, o.name from projects as p 
+        $data =  DB::select('select p_a.id, p_a.pay_date, p_a.amount, p_a.state, p_a.order_id, o.name from projects as p
         inner join orders as o on p.id = o.project_id
         inner join payment_agreement as p_a on o.id = p_a.order_id
         where o.project_id = ? and p_a.pay_date between ? and ? ',[$project_id, $first_date, $end_date]);
