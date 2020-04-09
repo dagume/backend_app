606f298b874d5fe4e6685f2eb643ac6866884960
dagume <guechadaniel@gmail.com>
2020-04-09 13:32:42 -0500

    implementacion update profile, politicas y permisios Contact v1

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -16,6 +16,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
         'App\Model\Project' => 'App\Policies\ProjectPolicy',
+        'App\Model\User' => 'App\Policies\UserPolicy',
     ];
 
     /**
