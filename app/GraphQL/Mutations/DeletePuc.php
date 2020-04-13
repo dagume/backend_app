0cc2750de6d81f2ba9fef8b9a8b5dde3c4602c0c
dagume <guechadaniel@gmail.com>
2020-04-12 23:27:05 -0500

    delete puc con validacion

---
+++ app/GraphQL/Mutations/DeletePuc.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use App\Puc;
+
+class DeletePuc
+{
+    /**
+     * Return a value for the field.
+     *
+     * @param  null  $rootValue Usually contains the result returned from the parent field. In this case, it is always `null`.
+     * @param  mixed[]  $args The arguments that were passed into the field.
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Arbitrary data that is shared between all fields of a single query.
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+     * @return mixed
+     */
+    public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        try
+		{
+			$puc = Puc::find($args['id']);
+			$puc->delete();
+		}
+        catch (\Illuminate\Database\QueryException $e)
+        {
+			return [
+                'puc' => null,
+                'message' => 'Esta cuenta no se puede eliminar',
+                'type' => 'Failed'
+            ];
+        }
+        return [
+            'puc' => $puc,
+            'message' => 'Cuenta eliminada exitosamente',
+            'type' =>'Successful'
+        ];    }
+}
