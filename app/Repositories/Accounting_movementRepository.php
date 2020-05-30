f8031faafed2f938f791765430529344a9519aa1
dagume <guechadaniel@gmail.com>
2020-05-30 10:53:05 -0500

    Arreglo de queries de reportes

--- app/Repositories/Accounting_movementRepository.php
+++ app/Repositories/Accounting_movementRepository.php
@@ -20,12 +20,12 @@ class Accounting_movementRepository extends BaseRepository
     public function partner_contributions($project_id, $role_id)
     {   //reporte de aportes de socios al proyecto
         $data = DB::select('Select * from accounting_movements Where project_id = ? and origin_role_id = ?', [$project_id, $role_id]);
-        return $data;
+        return $this->getObjects($data);
     }
     public function a_partner_contributions($project_id, $origin_id, $role_id)
     {   //reporte de aportes de un socio al proyecto
         $data = DB::select('select * from accounting_movements where project_id = ? and origin_id = ? and origin_role_id = ?', [$project_id, $origin_id, $role_id]);
-        return $data;
+        return $this->getObjects($data);
     }
     public function partner_contributions_and_refunds($project_id, $contact_id, $role_id, $projectContact_id)
     {   //Aportes y reembolsos de socio o prestamista en un proyecto
@@ -34,22 +34,22 @@ class Accounting_movementRepository extends BaseRepository
         and (origin_id = ? and destination_id = ? and origin_role_id = ?)
         or (origin_id = ? and destination_id = ? and destination_role_id = ?)',
         [$project_id, $contact_id, $projectContact_id, $role_id, $projectContact_id, $contact_id, $role_id]);
-        return $data;
+        return $this->getObjects($data);
     }
     public function project_expenses($project_id, $origin_id, $role_id)
     {   //reporte de Registro de egresos en general del proyecto
         $data = DB::select('select * from accounting_movements where project_id = ? and origin_id = ? and origin_role_id = ?', [$project_id, $origin_id, $role_id]);
-        return $data;
+        return $this->getObjects($data);
     }
     public function project_expenses_for_concept($project_id, $origin_id, $role_id, $puc_id)
     {   //reporte de Registro de egresos por concepto del proyecto
         $data = DB::select('select * from accounting_movements where project_id = ? and origin_id = ? and origin_role_id = ? and puc_id = ?', [$project_id, $origin_id, $role_id, $puc_id]);
-        return $data;
+        return $this->getObjects($data);
     }
     public function project_income($project_id, $destination_id, $role_id)
     {   //reporte de Registro de ingresos del proyecto
         $data = DB::select('select * from accounting_movements where project_id = ? and destination_id = ? and destination_role_id = ?', [$project_id, $destination_id, $role_id]);
-        return $data;
+        return $this->getObjects($data);
     }
 }
 
