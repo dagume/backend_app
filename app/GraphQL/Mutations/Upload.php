589243432fc897aa2a0dccce3ff029c7fa127c55
dagume <guechadaniel@gmail.com>
2020-06-03 00:34:29 -0500

    File upload con GraphQL v1

---
+++ app/GraphQL/Mutations/Upload.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+class Upload
+{
+    /**
+     * Upload a file, store it on the server and return the path.
+     *
+     * @param  mixed  $root
+     * @param  mixed[]  $args
+     * @return string|null
+     */
+    public function resolve($root, array $args): ?string
+    {
+        /** @var \Illuminate\Http\UploadedFile $file */
+        $file = $args['file'];
+
+        return $file->storePublicly('uploads');
+    }
+}
+//
+//namespace App\GraphQL\Mutations;
+//
+//use GraphQL\Type\Definition\ResolveInfo;
+//use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+//
+//class Upload
+//{
+//    /**
+//     * Return a value for the field.
+//     *
+//     * @param  null  $rootValue Usually contains the result returned from the parent field. In this case, it is always `null`.
+//     * @param  mixed[]  $args The arguments that were passed into the field.
+//     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Arbitrary data that is shared between all fields of a single query.
+//     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+//     * @return mixed
+//     */
+//    public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+//    {
+//        // TODO implement the resolver
+//    }
+//}
