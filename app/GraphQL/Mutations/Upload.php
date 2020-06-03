5c150f1fe4346d7c67e74c0f2ecc6da2f5b857db
dagume <guechadaniel@gmail.com>
2020-06-03 10:46:29 -0500

    File upload con GraphQL v5

--- app/GraphQL/Mutations/Upload.php
+++ app/GraphQL/Mutations/Upload.php
@@ -21,8 +21,8 @@ class Upload
         /** @var \Illuminate\Http\UploadedFile $file */
         $file = $args['file'];
         //$file->save(storage_path('pdf').'/'.'por_graphql');
-        Storage::put('file', $file);
-        return $file->storePublicly('uploads');
+        return Storage::put('file', $file);
+        //return $file->storePublicly('uploads');
     }
 }
 //
