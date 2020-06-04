2cdc479a2549f5c361ab82f70b6956c00d74184b
dagume <guechadaniel@gmail.com>
2020-06-04 01:22:37 -0500

    Files upload con GraphQL in activities v31

--- app/GraphQL/Mutations/Upload.php
+++ app/GraphQL/Mutations/Upload.php
@@ -59,24 +59,24 @@ class Upload
                 Storage::putFileAs(
                    'files', $files_gra, $args['names'][$key]
                 ); //Guardamos archivo en el Storage
-            }
-            $files = Storage::files('files');      // Estamos cargando los archivos que estan en el Storage, traemos todos los documentos
-            foreach ($files as $key => $file) {     // recorremos cada uno de los file encontrados
-                $name_file = explode( '/', $file);
-                $read = Storage::get($file);                    // leemos el contenido del PDF
-                $archivo = $filesystem->write(end($name_file), $read);    // Guarda el archivo en el drive
-                $file_id = $filesystem->getMetadata(end($name_file));     // get data de file en Drive
-                Storage::delete('files/'.$args['names'][$key]);   //eliminamos el file del Storage, ya que se encuentra cargado en el drive
+                $files = Storage::files('files');      // Estamos cargando los archivos que estan en el Storage, traemos todos los documentos
+                foreach ($files as $key => $file) {     // recorremos cada uno de los file encontrados
+                    $name_file = explode( '/', $file);
+                    $read = Storage::get($file);                    // leemos el contenido del PDF
+                    $archivo = $filesystem->write(end($name_file), $read);    // Guarda el archivo en el drive
+                    $file_id = $filesystem->getMetadata(end($name_file));     // get data de file en Drive
+                    Storage::delete('files/'.$args['names'][$key]);   //eliminamos el file del Storage, ya que se encuentra cargado en el drive
 
-                //Para subir documento de actividad
-                $doc_ref_file->parent_document_id = DB::table('document_reference')->where('project_id', $args['project_id'])->where('activity_id', $args['activity_id'])->first()->id;
-                $doc_ref_file->name = $args['names'][$key];
-                $doc_ref_file->is_folder = 0; // 0 = Tipo File, 1 = Tipo Folder
-                $doc_ref_file->activity_id = $args['activity_id'];
-                $doc_ref_file->project_id = $args['project_id'];
-                $doc_ref_file->module_id = 1; //id 1 pertenece al modulo activity
-                $doc_ref_file->drive_id =  $file_id['path'];
-                $doc_ref_file->save();
+                    //Para subir documento de actividad
+                    $doc_ref_file->parent_document_id = DB::table('document_reference')->where('project_id', $args['project_id'])->where('activity_id', $args['activity_id'])->first()->id;
+                    $doc_ref_file->name = $args['names'][$key];
+                    $doc_ref_file->is_folder = 0; // 0 = Tipo File, 1 = Tipo Folder
+                    $doc_ref_file->activity_id = $args['activity_id'];
+                    $doc_ref_file->project_id = $args['project_id'];
+                    $doc_ref_file->module_id = 1; //id 1 pertenece al modulo activity
+                    $doc_ref_file->drive_id =  $file_id['path'];
+                    $doc_ref_file->save();
+                }
             }
         }else{
             return [
