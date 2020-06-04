ab50f4df9eeba7f2c5a1b87b247494e307a0cf5b
dagume <guechadaniel@gmail.com>
2020-06-04 00:31:00 -0500

    Files upload con GraphQL in activities v29

--- app/GraphQL/Mutations/Upload.php
+++ app/GraphQL/Mutations/Upload.php
@@ -44,7 +44,7 @@ class Upload
      * @return string|null
      */
     public function resolve($root, array $args)
-    { 
+    {
         /** @var \Illuminate\Http\UploadedFile $file */
         //Storage::delete('files/jwt.txt');
         //dd(Storage::files('files'));
@@ -54,28 +54,31 @@ class Upload
             //dd($this->document_referenceRepo->getFolderSubActivity($args['project_id'], $args['activity_id'])->drive_id);
             $adapter    = new GoogleDriveAdapter(Conection_Drive(), $this->document_referenceRepo->getFolderSubActivity($args['project_id'], $args['activity_id'])->drive_id); //Caarpeta donde vamos a guardar el documento
             $filesystem = new Filesystem($adapter);
-            $file_graphql = $args['file'];//Archivo enviado
-            $path = Storage::putFileAs(
-               'files', $file_graphql, $args['name']
-            ); //Guardamos archivo en el Storage
+            $files_graphql = $args['files'];//Archivos enviados
+            foreach ($files_graphql as $key => $files_gra) {
+                Storage::putFileAs(
+                   'files', $file_graphql, $args['names'][$key]
+                ); //Guardamos archivo en el Storage
+            }
             $files = Storage::files('files');      // Estamos cargando los archivos que estan en el Storage, traemos todos los documentos
-            foreach ($files as $file) {     // recorremos cada uno de los file encontrados
+            foreach ($files as $key => $file) {     // recorremos cada uno de los file encontrados
                 $name_file = explode( '/', $file);
                 $read = Storage::get($file);                    // leemos el contenido del PDF
                 $archivo = $filesystem->write(end($name_file), $read);    // Guarda el archivo en el drive
                 $file_id = $filesystem->getMetadata(end($name_file));     // get data de file en Drive
-                Storage::delete('files/'.$args['name']);   //eliminamos el file del Storage, ya que se encuentra cargado en el drive
+                Storage::delete('files/'.$args['names'][$key]);   //eliminamos el file del Storage, ya que se encuentra cargado en el drive
+
+                //Para subir documento de actividad
+                $doc_ref_file->parent_document_id = DB::table('document_reference')->where('project_id', $args['project_id'])->where('activity_id', $args['activity_id'])->first()->id;
+                $doc_ref_file->name = $args['names'][$key];
+                $doc_ref_file->is_folder = 0; // 0 = Tipo File, 1 = Tipo Folder
+                $doc_ref_file->activity_id = $args['activity_id'];
+                $doc_ref_file->project_id = $args['project_id'];
+                $doc_ref_file->module_id = 1; //id 1 pertenece al modulo activity
+                $doc_ref_file->drive_id =  $file_id['path'];
+                //$doc_ref_file->drive_id =  'rtyfgggggg';
+                $doc_ref_file->save();
             }
-            //Para subir documento de actividad
-            $doc_ref_file->parent_document_id = DB::table('document_reference')->where('project_id', $args['project_id'])->where('activity_id', $args['activity_id'])->first()->id;
-            $doc_ref_file->name = $args['name'];
-            $doc_ref_file->is_folder = 0; // 0 = Tipo File, 1 = Tipo Folder
-            $doc_ref_file->activity_id = $args['activity_id'];
-            $doc_ref_file->project_id = $args['project_id'];
-            $doc_ref_file->module_id = 1; //id 1 pertenece al modulo activity
-            $doc_ref_file->drive_id =  $file_id['path'];
-            //$doc_ref_file->drive_id =  'rtyfgggggg';
-            $doc_ref_file->save();
         }else{
             return [
                 'message' => 'No se pudo cargar ningun archivo, intente de nuevo',
