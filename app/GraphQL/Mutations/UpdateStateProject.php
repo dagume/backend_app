dfdb903fcfc9a7fa10e7d310d2e7e8eaab719886
dagume <guechadaniel@gmail.com>
2020-06-05 23:10:41 -0500

    document reference en actividades de suspension

--- app/GraphQL/Mutations/UpdateStateProject.php
+++ app/GraphQL/Mutations/UpdateStateProject.php
@@ -7,6 +7,8 @@ use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 use App\Repositories\ProjectRepository;
 use App\Repositories\ActivityRepository;
 use App\Repositories\Document_referenceRepository;
+use App\Document_reference;
+
 use DB;
 
 class UpdateStateProject
@@ -43,10 +45,30 @@ class UpdateStateProject
                 //guarda registro de la nueva actividad
                 $activity_again = $this->activityRepo->create(StoreActivity($args['id'], 'Acta de reinicio '.date("d-m-Y"), $again_Act_folder->id));
 
+                $doc_ref_activity = new Document_reference; // aqui vamos a guardar la estructura de las carpetas creadas
+                $doc_ref_activity->parent_document_id = $this->documentRepo->getFolderParentActivity($args['id'])->id;    //Buscamos el folder donde vamos a guardar la actividad
+                $doc_ref_activity->name ='Acta de reinicio '.date("d-m-Y");
+                $doc_ref_activity->is_folder = 1; // 0 = Tipo File, 1 = Tipo Folder
+                $doc_ref_activity->activity_id =  $activity_again->id;
+                $doc_ref_activity->project_id = $args['id'];
+                $doc_ref_activity->module_id = 1; //id 1 pertenece al modulo Activity
+                $doc_ref_activity->drive_id = $again_Act_folder->id;
+                $doc_ref_activity->save();
+
                 //Hacemos conexion con el drive y creamos el folder de Actividad. Metodos en Helper.php
                 $poliza_Act_folder = Conection_Drive()->files->create(Create_Folder('Polizas de reinicio '.date("d-m-Y"), $activity_folder->drive_id), ['fields' => 'id']);
                 //guarda registro de la nueva actividad
                 $activity_poliza = $this->activityRepo->create(StoreActivity($args['id'], 'Polizas de reinicio '.date("d-m-Y"), $poliza_Act_folder->id));
+
+                $doc_ref_activity = new Document_reference; // aqui vamos a guardar la estructura de las carpetas creadas
+                $doc_ref_activity->parent_document_id = $this->documentRepo->getFolderParentActivity($args['id'])->id;    //Buscamos el folder donde vamos a guardar la actividad
+                $doc_ref_activity->name ='Polizas de reinicio '.date("d-m-Y");
+                $doc_ref_activity->is_folder = 1; // 0 = Tipo File, 1 = Tipo Folder
+                $doc_ref_activity->activity_id =  $activity_poliza->id;
+                $doc_ref_activity->project_id = $args['id'];
+                $doc_ref_activity->module_id = 1; //id 1 pertenece al modulo Activity
+                $doc_ref_activity->drive_id = $poliza_Act_folder->id;
+                $doc_ref_activity->save();
             }
 
             if ($args['state'] === 4 && $this->projectRepo->find($args['id'])->state != 4) {
@@ -54,6 +76,16 @@ class UpdateStateProject
                 $suspensionAct_folder = Conection_Drive()->files->create(Create_Folder('Acta de suspensión '.date("d-m-Y"), $activity_folder->drive_id), ['fields' => 'id']);
                 //guarda registro de la nueva actividad
                 $activity = $this->activityRepo->create(StoreActivity($args['id'], 'Acta de suspensión '.date("d-m-Y"), $suspensionAct_folder->id));
+
+                $doc_ref_activity = new Document_reference; // aqui vamos a guardar la estructura de las carpetas creadas
+                $doc_ref_activity->parent_document_id = $this->documentRepo->getFolderParentActivity($args['id'])->id;    //Buscamos el folder donde vamos a guardar la actividad
+                $doc_ref_activity->name ='Acta de suspensión '.date("d-m-Y");
+                $doc_ref_activity->is_folder = 1; // 0 = Tipo File, 1 = Tipo Folder
+                $doc_ref_activity->activity_id =  $activity->id;
+                $doc_ref_activity->project_id = $args['id'];
+                $doc_ref_activity->module_id = 1; //id 1 pertenece al modulo Activity
+                $doc_ref_activity->drive_id = $suspensionAct_folder->id;
+                $doc_ref_activity->save();
             }
 
             $project = $this->projectRepo->update($args['id'], $args);
