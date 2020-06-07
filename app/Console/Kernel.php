a8ef4257219d242c3a051157424dee1e6c7b259e
dagume <guechadaniel@gmail.com>
2020-06-07 10:07:46 -0500

    volumen de backup postgres

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('daily:payment')->everyTenMinutes();
         // $schedule->command('inspire')
         //          ->hourly();
     }
