ab9e7538c595bc98f028aa0037a3c422b2f3d8ad
dagume <guechadaniel@gmail.com>
2020-06-10 00:16:10 -0500

    sheduler daily run

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('daily:payment')->everyTenMinutes();
+        $schedule->command('daily:payment')->daily();
         // $schedule->command('inspire')
         //          ->hourly();
     }
